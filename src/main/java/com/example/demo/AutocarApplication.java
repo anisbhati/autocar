42aad9a3c36a253c00cece1d47453db390221df5
anisbhati <54569086+anisbhati@users.noreply.github.com>
2024-05-28 10:34:47 +0530

    1st commit

---
+++ src/main/java/com/example/demo/AutocarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocarApplication.class, args);
+	}
+
+}
